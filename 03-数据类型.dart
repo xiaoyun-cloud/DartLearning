95b858b3d5a2bf0c12fa71f0430e883beb53bf8c
mac <yunqing-cloud@qq.com>
2024-12-30 22:48:50 +0800

    更新了03的部分内容

--- 03-数据类型.dart
+++ 03-数据类型.dart
@@ -1,1 +1,15 @@
-˝003;
+//数据类型String,单双引号都可以,三个引号可以包含换行符
+//需要阅读常见的API文档
+
+RegExp(r'正则表达式')
+RegExp(r'\d+')//匹配数字,\d表示数字,+,表示一个或多个
+
+//下面是验证siring,框选代码后再运行,否则会报错
+void main(){
+    //声明字符串
+    var str1 = 'Hello';
+    print(str1);
+    String str2 = "你好世界";//通过string关键字声明字符串
+    print(str2);
+}
+
