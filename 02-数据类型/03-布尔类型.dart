ee6bae586d53c21bdd048309a4dd2efac63f5fad
mac <yunqing-cloud@qq.com>
2025-01-01 20:31:42 +0800

    新增了03的部份代码

--- 02-数据类型/03-布尔类型.dart
+++ 02-数据类型/03-布尔类型.dart
@@ -1,3 +1,17 @@
 // boolean类型(布尔类型)
-  //布尔类型只有两个值:true和false
-  //对变量进行判断时,要显示其真假值,就需要用到布尔类型
+//布尔类型只有两个值:true和false
+//对变量进行判断时,要显示其真假值,就需要用到布尔类型
+void main() {
+  //声明布尔类型变量
+  bool flag = true;
+  print(flag);
+  bool flag2 = false;
+  print(flag2);
+  //布尔类型变量可以作为条件判断
+  var flag3;
+  if (flag3 == null) {
+    print("真");
+  } else {
+    print("假");
+  }
+}
