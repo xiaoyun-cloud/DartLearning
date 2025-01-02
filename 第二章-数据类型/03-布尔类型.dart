b4998946dbe410f655cf35a8f6ca764b48ee7707
mac <yunqing-cloud@qq.com>
2025-01-02 15:52:37 +0800

    完成了03布尔的更新

--- 第二章-数据类型/03-布尔类型.dart
+++ 第二章-数据类型/03-布尔类型.dart
@@ -13,7 +13,10 @@ void main() {
   var n1 = 0 / 0;
   print(n1); //输出为NaN,是not a number缩写,表示不是一个数字
   print(n1.isNaN); //判断n1是否为非数字,是则输出true
-  print(n1.)
+  //以下是一些特殊的判断
+  print(n1.isInfinite); //判断n1是否为无穷大,是则输出true
+  print(n1.isNegative); //判断n1是否为负数,是则输出true
+  print(n1.isFinite); //判断n1是否为有限数,是则输出true
+  print(n1.isEven); //判断n1是否为偶数,是则输出true
+  print(n1.isOdd); //判断n1是否为奇数,是则输出true
 }
-
-
