b6eadea936b0a9a5d09b432b0c0553d07281141c
mac <yunqing-cloud@qq.com>
2025-01-11 00:03:14 +0800

    feat(dart): 添加数据库抽象类及其实现示例

---
+++ 第四章-接口/lib/DB.dart
@@ -0,0 +1,13 @@
+abstract class DB {
+  //数据库连接信息以及增删改查接口
+
+  late String uri;
+  add(String data);
+  save();
+  delete();
+  update();
+}
+
+//代码版本:1.0
+//代码解释:定义了一个抽象类DB,里面包含了数据库连接信息以及增删改查接口
+//end
